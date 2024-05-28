9c25df610d56ed2d183ff01190d0436e33471aa4
amolg98 <amol.g98@gmail.com>
2024-05-28 19:30:25 -0400

    Update browserrouter with basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Home from "./components/home/Home";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MembershipAndFees from "./components/membership-and-fees/MembershipAndFees";
 import NavBar from "./components/headerbar/NavBar";
 
 function App() {
   return (
     <div className="text-center w-full">
-      <HashRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/membership-and-fees" element={<MembershipAndFees />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
