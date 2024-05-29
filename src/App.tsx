60718a25f8b94f9b2b733be62a220d46c623a39d
amolg98 <amol.g98@gmail.com>
2024-05-28 21:36:03 -0400

    Test 404.html changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./components/home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MembershipAndFees from "./components/membership-and-fees/MembershipAndFees";
 import NavBar from "./components/headerbar/NavBar";
+import TestPage from "./components/TestPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/test-page" element={<TestPage />} />
           <Route path="/membership-and-fees" element={<MembershipAndFees />} />
         </Routes>
       </BrowserRouter>
