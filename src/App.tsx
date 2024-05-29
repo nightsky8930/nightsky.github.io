8cc1840dfa7ded175a881940e8d71e70be0608fd
amolg98 <amol.g98@gmail.com>
2024-05-28 22:07:50 -0400

    Try HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./components/home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import MembershipAndFees from "./components/membership-and-fees/MembershipAndFees";
 import NavBar from "./components/headerbar/NavBar";
 import TestPage from "./components/TestPage";
@@ -8,14 +8,14 @@ import TestPage from "./components/TestPage";
 function App() {
   return (
     <div className="text-center w-full">
-      <BrowserRouter>
+      <HashRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/test-page" element={<TestPage />} />
           <Route path="/membership-and-fees" element={<MembershipAndFees />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
