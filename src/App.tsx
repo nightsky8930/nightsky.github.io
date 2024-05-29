0ea4e67bd06a43a9bf1cacb5bfa657d1e496008f
amolg98 <amol.g98@gmail.com>
2024-05-28 22:14:02 -0400

    Try the reload fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./components/home/Home";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MembershipAndFees from "./components/membership-and-fees/MembershipAndFees";
 import NavBar from "./components/headerbar/NavBar";
 import TestPage from "./components/TestPage";
@@ -8,14 +8,14 @@ import TestPage from "./components/TestPage";
 function App() {
   return (
     <div className="text-center w-full">
-      <HashRouter>
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/test-page" element={<TestPage />} />
           <Route path="/membership-and-fees" element={<MembershipAndFees />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
