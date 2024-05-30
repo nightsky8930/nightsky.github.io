ee3d53009a44b757b4b21dcaf98ff1c088c5624d
amolg98 <amol.g98@gmail.com>
2024-05-30 01:38:02 -0400

    Add google forms for liability waiver form

--- src/components/membership-and-fees/WaiverForm.tsx
+++ src/components/membership-and-fees/WaiverForm.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-
 const WaiverForm = () => {
+
+  const handleShowForm = () => {
+    const googleFormUrl = 'https://forms.gle/Vwu95roNkhn9fpW68';
+    const newWindow = window.open(googleFormUrl, '_blank', 'noopener,noreferrer');
+    if (newWindow) newWindow.opener = null;
+  };
+
   return(
     <div>
       <div className="flex-grow flex flex-col items-center px-4 py-14">
         <h1 className="text-xl mb-6">New Members will have to fill out a waiver form before start playing with the club</h1>
         <button
             className="flex-grow flex bg-green-500 hover:bg-green-700 items-center justify-center rounded-full text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline"
+            onClick={handleShowForm}
             type="button"
           >
             CLICK HERE TO SUBMIT WAIVER FORM
